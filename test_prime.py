43f763477bab60a33bb3042fc8dc4eab5005aa2c
Noam-Gruber <gruber.noam@gmail.com>
2025-02-19 22:15:00 +0200

    check test_prime

--- test_prime.py
+++ test_prime.py
@@ -1,6 +1,7 @@
 import pytest
 from prime import is_prime
 
+
 def test_prime():
     assert is_prime(2) is True
     assert is_prime(3) is True
@@ -11,6 +12,7 @@ def test_prime():
     assert is_prime(1) is False
     assert is_prime(0) is False
     assert is_prime(-5) is False
+
 
 if __name__ == "__main__":
     pytest.main()

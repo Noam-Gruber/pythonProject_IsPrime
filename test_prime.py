3749c90266922f3feba95722add91bf6508848f0
Noam Gruber <noam.gruber@qualitestgroup.com>
2025-02-19 22:10:02 +0200

    check test_prime

---
+++ test_prime.py
@@ -0,0 +1,16 @@
+import pytest
+from prime import is_prime
+
+def test_prime():
+    assert is_prime(2) is True
+    assert is_prime(3) is True
+    assert is_prime(4) is False
+    assert is_prime(5) is True
+    assert is_prime(29) is True
+    assert is_prime(30) is False
+    assert is_prime(1) is False
+    assert is_prime(0) is False
+    assert is_prime(-5) is False
+
+if __name__ == "__main__":
+    pytest.main()
